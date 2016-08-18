96754ad37d1ede305727de303fca2d48676ae9ad
thealemaster <jhuston@pillartechnology.com>
2016-08-17 21:56:31 -0400

    Added logic to convert to/from decimal

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # roman
+
+This is my attempt to complete the Programming Exercise for
+the Falcon project. A couple of caveats to throw out there:
+- My initial check-in to github is a minimal set of files
+  that compile and create a library with no logic, just to
+  set up the structure of the project
+- My first handful of tests were done to give me some
+  familiarity with the check testing framework, which is
+  new to me. So when you see a deviation of the solution
+  after the first tests, that's why. My intent was always
+  to solve the problem by doing string-to-integer-and-back
+- My other goal was to be minimalistic. There are a lot of
+  other things I could have done to explore all the nuances
+  of roman arithmetic. Instead, I tried to stick to the
+  rules of the exercise and not go too far off script
--- lib/roman_calculator.c
+++ lib/roman_calculator.c
@@ -4,18 +4,59 @@
 #include <stdio.h>
 #include <string.h>
 
+int convertRomanNumeralToDecimal (char romanNumeralToConvert) 
+{
+  int convertedRomanNumeral = 0;
+
+  if (romanNumeralToConvert = 'I') {
+    convertedRomanNumeral = 1;
+  }
+
+  return (convertedRomanNumeral);
+}
+
+void convertDecimalToRomanNumeral (int decimalToConvert, char *convertedNumeral)
+{
+  int remainingDecimalToConvert = decimalToConvert;
+  int convertedNumeralArrayIndex = 0;
+
+  while (remainingDecimalToConvert > 0) {
+    /* If the number is less than 4, we're just dealing with I's */
+    if (remainingDecimalToConvert < 4) {
+      convertedNumeral [convertedNumeralArrayIndex] = 'I';
+      remainingDecimalToConvert = remainingDecimalToConvert - 1;
+      convertedNumeralArrayIndex++;
+    }
+  }
+
+}
+
 int add_roman_numerals (char *firstNumeral, char *secondNumeral, char*resultNumeral)
 {
 
-  if (toupper(firstNumeral [0]) == 'I' && toupper(firstNumeral [1]) == 'I' && toupper( secondNumeral [0] == 'I')) {
-      strcpy (resultNumeral, "III");
+  int firstConvertedNumeral = 0, secondConvertedNumeral = 0;
+  int numeralArrayIndex = 0;
+
+  while (toupper(firstNumeral[numeralArrayIndex])) {
+    firstConvertedNumeral = firstConvertedNumeral + convertRomanNumeralToDecimal (toupper(firstNumeral[numeralArrayIndex]));
+    numeralArrayIndex++;
   }
-  else if (toupper(firstNumeral[0]) == 'I' && toupper(secondNumeral [0]) == 'I' && toupper(secondNumeral [1]) == 'I') {
-      strcpy (resultNumeral, "III");
+
+  numeralArrayIndex = 0;
+
+  while (toupper(secondNumeral[numeralArrayIndex])) {
+    secondConvertedNumeral = secondConvertedNumeral + convertRomanNumeralToDecimal (toupper(secondNumeral[numeralArrayIndex]));
+    numeralArrayIndex++;
   }
-  else if (toupper (firstNumeral [0]) == 'I' && toupper (secondNumeral [0]) == 'I') {
-    strcpy (resultNumeral, "II");
+
+  /* Initialize the output string to NULLs */
+  numeralArrayIndex = 0;
+
+  while (numeralArrayIndex < 10) {
+    resultNumeral [numeralArrayIndex] = '\0';
+    numeralArrayIndex++;
   }
+  convertDecimalToRomanNumeral (firstConvertedNumeral + secondConvertedNumeral, resultNumeral);
 
   return (1);
 }
