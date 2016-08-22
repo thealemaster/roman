2dd05d62052059fa888368c38b74c87ed9d9fb7f
thealemaster <jhuston@pillartechnology.com>
2016-08-22 19:26:42 -0400

    Added include guard to check_roman.h

--- include/calculate_roman.h
+++ include/calculate_roman.h
@@ -2,9 +2,14 @@
   Header file to hold common roman numeral coding exercise goodness
 */
 
+#ifndef CHECK_ROMAN_H
+#define CHECK_ROMAN_H
+
 #define MAX_ROMAN_NUMERAL_VALUE 3999
 #define MAX_ROMAN_NUMERAL_STRING_SIZE 20
 
 extern int add_roman_numerals (char *firstNumeral, char *secondNumeral, char*resultNumeral);
 
 extern int subtract_roman_numerals (char *firstNumeral, char *secondNumeral, char*resultNumeral);
+
+#endif /*CHECK_ROMAN_H */
